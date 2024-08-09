b8229d706d6ed4aa350da53f5d5c53d644f11333
pranayrdandekar7 <pranayrdandekar7@gmail.com>
2024-08-09 23:42:33 +0530

    frontend basic setup and routing implememnted

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {RouterProvider,createBrowserRouter} from "react-router-dom"
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Home from './views/Home/Home';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const router= createBrowserRouter([
+  {
+    path:"/",
+    element: <Home/>
+  },
+  {
+    path:"*",
+    element: <h1>404 Not Found</h1>
+  },
+ 
+])
+
+root.render(<RouterProvider router={router}/>) ;
+
+
+
