22ecc09dd65a4642c5541b2ef306e0eb3a1b2c52
pranayrdandekar7 <pranayrdandekar7@gmail.com>
2024-09-26 14:08:06 +0530

    adding media qeries for terms and conditions

--- client/src/views/TermsAndConditions/TermsAndConditions.js
+++ client/src/views/TermsAndConditions/TermsAndConditions.js
@@ -19,28 +19,28 @@ const TermsAndConditions = () => {
 
   return (
     <div className="terms-container">
-      <h1 className="text-primary">Terms and Conditions</h1>
-      <span> 
+      <h1 className="text-primary heading">Terms and Conditions -:</h1>
+      <span className="conditions"> 
         Welcome to our Expense Tracker! By using our service, you agree to the following terms and conditions:
       </span>
-      <h2 className="text-primary">1. Acceptance of Terms</h2>
-      <span>
+      <h2 className="text-primary">1. Acceptance of Terms :</h2>
+      <span className="conditions">
         By accessing or using our website and services, you agree to comply with and be bound by these terms and conditions.
-      </span>
-      <h2 className="text-primary">2. Use of Service</h2>
-      <span>
+      </span >
+      <h2 className="text-primary">2. Use of Service :</h2>
+      <span className="conditions">
         You agree to use our service only for lawful purposes and in accordance with our guidelines.
       </span>
-      <h2 className="text-primary">3. Privacy Policy</h2>
-      <span>
+      <h2 className="text-primary">3. Privacy Policy :</h2>
+      <span className="conditions">
         We value your privacy. Our Privacy Policy outlines how we collect, use, and protect your information.
-      </span>
-      <h2 className="text-primary">4. Changes to Terms</h2>
-      <span>
+      </span >
+      <h2 className="text-primary">4. Changes to Terms :</h2>
+      <span className="conditions">
         We may update these terms from time to time. You are encouraged to review these terms periodically for any changes.
       </span>
-      <h2 className="text-primary">5. Contact Us</h2>
-      <span>
+      <h2 className="text-primary">5. Contact Us :</h2>
+      <span className="conditions">
         If you have any questions or concerns about these terms, please contact us at 
         <a href="mailto:pranayrdandekar7@gmail.com" style={{textDecoration:"none"}}> pranayrdandekar7@gmail.com </a>
       </span>
