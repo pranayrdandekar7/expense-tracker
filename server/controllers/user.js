7ec959f67393f43d3dbcbb2b2a3a2401c17d3512
pranayrdandekar7 <pranayrdandekar7@gmail.com>
2024-10-07 13:53:04 +0530

    mail send problem fix and ui change

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -3,96 +3,97 @@ import Email from "./../models/SendMail.js"
 import nodemailer from "nodemailer"
 import { textTemplate } from '../config.js';
 
-const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
-  auth: {
-      user: "pranaydandekar6@gmail.com",
-      pass: "ktti bgjo jsad fxly"
-  }
-});
 
 const postSignup = async (req, res) => {
-   const { fullname, email, password, dob } = req.body ;
+   const { fullname, email, password, dob } = req.body;
 
    const user = new User({
       fullname: fullname,
       email: email,
       password: password,
-      dob: new Date (dob) 
-   }) ;
+      dob: new Date(dob)
+   });
 
-   try{
-   const savedUser = await user.save();
-   
- const html= textTemplate.replace("####user###", savedUser.fullname)
- const subject= "Welcome to Expense Tracker"
+   try {
+      const savedUser = await user.save();
 
-   const mailresponse = await transporter.sendMail({
-     from: `"Expense Tracker " <${process.env.SEND_EMAIL_ID}>`,
-     to:email,
-     subject:subject,
-     html: html
- })
+      const html = textTemplate.replace("####user###", savedUser.fullname)
+      const subject = "Welcome to Expense Tracker"
 
- const emailData =new Email({
-    to :email,
-    subject: subject,
-    html:html,
-    status:"sent",
-    user:savedUser._id
- })
+      const transporter = nodemailer.createTransport({
+         host: 'smtp.gmail.com',
+         port: 465,
+         secure: true,
+         auth: {
+            user: process.env.SEND_EMAIL_ID,
+            pass: process.env.SEND_EMAIL_PASS
+         }
+      });
 
- await emailData.save() ;
+      const mailresponse = await transporter.sendMail({
+         from: `"Expense Tracker " <${process.env.SEND_EMAIL_ID}>`,
+         to: email,
+         subject: subject,
+         html: html
+      })
+
+      const emailData = new Email({
+         to: email,
+         subject: subject,
+         html: html,
+         status: "sent",
+         user: savedUser._id
+      })
+
+      await emailData.save();
 
 
-   res.json ({
-      success : true,
-      message : `Signup successful`,
-      data : savedUser
-   })
+      res.json({
+         success: true,
+         message: `Signup successful`,
+         data: savedUser
+      })
 
    }
-   catch(e){
+   catch (e) {
       res.json({
-         success:false,
+         success: false,
          message: e.message,
-         data:null
+         data: null
 
       })
    }
 }
 
 
- const postLogin = async(req,res)=>{
+const postLogin = async (req, res) => {
 
-    const {email,password} =req.body
+   const { email, password } = req.body
 
- 
-    const user = await User.findOne({
- 
-       email:email,
-       password:password
-    });
- 
-    if(user){
-       res.json({
-          success:true,
-          message:`user login successfully`,
-          data:user
-       })
-    }
-    else{
-       res.json({
-          success:false,
-          message:`invalid credintial`,
-          data:null
-       })
-    }
- }
 
- export {
-    postSignup,
-    postLogin
- }
+   const user = await User.findOne({
+
+      email: email,
+      password: password
+   });
+
+   if (user) {
+      res.json({
+         success: true,
+         message: `user login successfully`,
+         data: user
+      })
+   }
+   else {
+      res.json({
+         success: false,
+         message: `invalid credintial`,
+         data: null
+      })
+   }
+}
+
+export {
+   postSignup,
+   postLogin
+}
--- server/index.js
+++ server/index.js
@@ -2,8 +2,6 @@ import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-
-
 dotenv.config();
 
 
